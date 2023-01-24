6bc1e8b4f1a09000c73a6ad215628a42833e14e2
jereycui2008 <jeremycuid@gmail.com>
2023-01-23 16:56:29 -0800

    I did own_adventure.

--- src/_01_else_if/_5_own_adventure/Own_adventure.java
+++ src/_01_else_if/_5_own_adventure/Own_adventure.java
@@ -27,14 +27,59 @@ public class Own_adventure {
 			if (option.equals("s")) {
 				JOptionPane.showMessageDialog(null, "It gets scared and runs away");
 				JOptionPane.showMessageDialog(null,
-						"You start to feel bad so you follow the animal inside a cave to find a whole family of them");
+						"You start to feel bad so you follow the animal inside a cave to find a dozen of them");
 				JOptionPane.showMessageDialog(null,
 						"They all look at you and start to walk slowely closer and closer to you");
 				JOptionPane.showMessageDialog(null,
 						"The animals start to growl.. and then pounce! You wake up again to find yourself where you first woke up (restart, go back to the start)");
 			}
+			if(option.equals("r")) {
+				JOptionPane.showMessageDialog(null, "You look the other way and start sprinting as fast as you can");
+				JOptionPane.showMessageDialog(null, "After a couple minutes you stop running and look back, that animal was gone");
+				JOptionPane.showMessageDialog(null, "Suddenly, you hear your stomach growl");
+				String  berries = JOptionPane.showInputDialog("Go look for something to eat (e) ignore it and move on (m) ");
+			
+			// (e1)
+			if(berries.equals("e")) {
+		    	JOptionPane.showMessageDialog(null, "You decide to look for food, so you searched the area and found some berries");
+		    	String food = JOptionPane.showInputDialog("eat the berries (p) look for something else to eat (i)");
 		    
+		    // (p)
+		    if(food.equals("p")) {
+		    	JOptionPane.showMessageDialog(null, "You start picking the berries");
+		    	JOptionPane.showMessageDialog(null, "After eating a couple handfulls, your stomach starts to hurt");
+		    	JOptionPane.showMessageDialog(null, "The pain gets worse and worse and then.. ");
+		    	JOptionPane.showMessageDialog(null, "POOF!!");
+		    	JOptionPane.showMessageDialog(null, "You turned into a hippo");
+		    }
+		    // (i)
+		    if(food.equals("i")) {
+		    	JOptionPane.showMessageDialog(null, "You suspect that the berries may be poisonous, you look for something else to eat");
+		    	JOptionPane.showMessageDialog(null, "You found a big apple tree with apples of multiple colors");
+		    	String colorfulApples = JOptionPane.showInputDialog("purple apple (P) orange apple (O) pink apple (I)");
+		    
+		    if(colorfulApples.equals("P")) {
+		    	JOptionPane.showMessageDialog(null, "The tasty apple fills you up but suddenly");
+		    	JOptionPane.showMessageDialog(null, "Your stomach starts to hurt and then..");
+		    	JOptionPane.showMessageDialog(null, "POOF!!");
+		    	JOptionPane.showMessageDialog(null, "You turned into a 6 legged unicorn");
+		    }
+		    if(colorfulApples.equals("O")){
+		    	JOptionPane.showMessageDialog(null, "The tasty apple fills you up but suddenly");
+		    	JOptionPane.showMessageDialog(null, "Your stomach starts to hurt and then..");
+		    	JOptionPane.showMessageDialog(null, "POOF!!");
+		    	JOptionPane.showMessageDialog(null, "You turned into a orange");
+		    }
+		    if(colorfulApples.equals("I")) {
+		    	JOptionPane.showMessageDialog(null, "The tasty apple fills you up but suddenly");
+		    	JOptionPane.showMessageDialog(null, "Your stomach starts to hurt and then..");
+		    	JOptionPane.showMessageDialog(null, "POOF!!");
+		    	JOptionPane.showMessageDialog(null, "You turned into a moth");
+		    }
 		}
 
 	}
+	}
+}
+}
 }

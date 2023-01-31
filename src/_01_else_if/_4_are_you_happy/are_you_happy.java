00a3a583d3401f458cf07cfe233d7c545115c008
jereycui2008 <jeremycuid@gmail.com>
2023-01-30 16:56:20 -0800

    I did do you want to be happy.

--- src/_01_else_if/_4_are_you_happy/are_you_happy.java
+++ src/_01_else_if/_4_are_you_happy/are_you_happy.java
@@ -1,5 +1,46 @@
 package _01_else_if._4_are_you_happy;
 
+import javax.swing.JOptionPane;
+
 public class are_you_happy {
 
+
+
+
+public static void main(String[] args) {
+	
+	String happy = JOptionPane.showInputDialog("Are you happy?");
+	if (happy.equals("yes")) {
+	 JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+	}
+	if (happy.equals("no")) {
+		String sad = JOptionPane.showInputDialog("Do you want to be happy?");
+	
+	if (sad.equals("no")) {
+		JOptionPane.showMessageDialog(null, "keep doing whatever you're doing");
+	}
+		
+	}
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
